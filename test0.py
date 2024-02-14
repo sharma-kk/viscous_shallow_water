f05fcb8f6647a9f4e2f7ec352b216e26ba0c3b8b
sharma-kk <sharma.uhh@gmail.com>
2024-02-14 18:08:20 +0100

    made changes in test0.py file, updated zonal vel, included alpha factor

--- test0.py
+++ test0.py
@@ -6,7 +6,7 @@ import math
 import time
 
 N = 64
-mesh = PeriodicRectangleMesh(7*N, N, 7, 1, direction="x")
+mesh = PeriodicRectangleMesh(7*N, N, 7, 1, direction="x") # L = 3891 km; R = 6370 km
 
 V1 = VectorFunctionSpace(mesh, "CG", 1)
 V2 = FunctionSpace(mesh, "CG", 1)
@@ -19,8 +19,8 @@ Ro = 10**(-1) ; Re = 10**3 ; B = 10**0 ; C = 10**(-2) ; Pe = 10**3
 
 # define initial condtions
 y0 = 1/14 ; y1 = 13/14
-
-u0_1 = conditional(Or(y <= y0, y >= y1), 0.0, exp(1/((y - y0)*(y - y1)))*exp(4/(y1 - y0)**2))
+alpha = 1.63 # 1/delta phi where phi is 35 degrees
+u0_1 = conditional(Or(y <= y0, y >= y1), 0.0, exp(alpha**2/((y - y0)*(y - y1)))*exp(4*alpha**2/(y1 - y0)**2))
 u0_2 = 0.0
 
 u0 = project(as_vector([u0_1, u0_2]), V1)
@@ -44,7 +44,7 @@ c0 = 0.01 ; c1 = 4 ;  c2 = 81 ; x_0 = 3.5; y_2 = 0.5
 
 h0_i = interpolate(h0_c + h0 , V2)
 h0_p = interpolate(c0*cos(math.pi*y/2)*exp(-c1*(x  - x_0)**2)*exp(-c2*(y - y_2)**2), V2)
-h0_f = interpolate(h0_c + h0 + c0*cos(math.pi*y/2)*exp(-c1*(x  - x_0)**2)*exp(-c2*(y - y_2)**2), V2) # perturbed initial height
+h0_f = interpolate(h0_c + h0 + h0_p, V2) # perturbed initial height
 
 outfile = File("./results/test.pvd")
 h0.rename("avg_h")
